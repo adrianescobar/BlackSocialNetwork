1a252f421e8eaff83553d2d87a583adb5c4a46ed
AdrianEscobar <adrian_080891@hotmail.com>
2011-08-10 15:33:31 -0700

    conexion

--- src/java/itla/edu/black/conexion/Conexion.java
+++ src/java/itla/edu/black/conexion/Conexion.java
@@ -4,38 +4,26 @@ package itla.edu.black.conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 public class Conexion {
     
     //referencia conexion
     static Connection con;
-    static Conexion clase;
+    static Conexion instancia = null;
     
     private Conexion(){
-        
         try{
-        
             Class.forName("com.mysql.jdbc.Driver");   
-            
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/black", "root", "");
-        
         }catch(Exception e){
-        
+            JOptionPane.showMessageDialog(null, "Error en la conexion!");
         }
-    
     }
-    
-    public static Connection getConexion(){
-    
-        if(clase==null){
-        
-            clase = new Conexion();
-        
+    public synchronized  static Connection getConexion(){
+         if(instancia==null){
+            instancia = new Conexion();
         }
-        
         return con;
-    
     }
-    
-    
 }
