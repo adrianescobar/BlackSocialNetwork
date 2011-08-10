c811b73a1ee215ce265c65468a777008964b7e89
AdrianEscobar <adrian_080891@hotmail.com>
2011-08-10 15:00:51 -0700

    subiendo proyecto

---
+++ src/java/itla/edu/black/conexion/Conexion.java
@@ -0,0 +1,41 @@
+
+package itla.edu.black.conexion;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    
+    //referencia conexion
+    static Connection con;
+    static Conexion clase;
+    
+    private Conexion(){
+        
+        try{
+        
+            Class.forName("com.mysql.jdbc.Driver");   
+            
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/black", "root", "");
+        
+        }catch(Exception e){
+        
+        }
+    
+    }
+    
+    public static Connection getConexion(){
+    
+        if(clase==null){
+        
+            clase = new Conexion();
+        
+        }
+        
+        return con;
+    
+    }
+    
+    
+}
