fc4b13bab9a86abc9b6628c34276604d2440f7f5
AdrianEscobar <adrian_080891@hotmail.com>
2011-08-10 21:51:51 -0700

    me estoy emocionando

--- src/java/itla/edu/black/data/photos/GuardarPhotosDB.java
+++ src/java/itla/edu/black/data/photos/GuardarPhotosDB.java
@@ -8,16 +8,16 @@ import javax.swing.JOptionPane;
 
 public class GuardarPhotosDB {
     
-    private Conexion con;
+    private Conexion con = null;
     private PreparedStatement consulta, insert = null;
     
     public GuardarPhotosDB(){
         try {
             this.con = Conexion.getInstance();
             consulta = con.getConexion().prepareStatement("select id_album from albums where id_usuario = (select id_usuario from usuario where email = ?) and nombre_album = 'Photos Perfil'");
-            insert = con.getConexion().prepareStatement("insert into photos(id_album,id_usuario,url) values(?,(select id_usuario from usuario where email = ?),?");
+            insert = con.getConexion().prepareStatement("insert into photos(id_album,id_usuario,url) values(?,(select id_usuario from usuario where email = ?),?)");
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error Guardando photos en bd");
+            JOptionPane.showMessageDialog(null, "Error Guardando photos en bd 1"+ex.getMessage());
         }
     }
     
@@ -35,9 +35,10 @@ public class GuardarPhotosDB {
             insert.setInt(1, id2);
             insert.setString(2, email);
             insert.setString(3, url);
+            JOptionPane.showMessageDialog(null, insert.toString());
             ret = insert.executeUpdate();
         }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Error Guardando photos en bd");
+            JOptionPane.showMessageDialog(null, "Error Guardando photos en bd 2"+e.getMessage());
         }
         return ret;
     }
